4161dd2cfad321546a188a0e66739503a1b81b95
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2026-09-16 23:54:30 -0700

    Remove obsolete guiding command handlers

--- python/lsst/ts/guider/guider_csc.py
+++ python/lsst/ts/guider/guider_csc.py
@@ -24,20 +24,15 @@ __all__ = ["GuiderCsc", "run_guider_csc"]
 import asyncio
 
 from lsst.ts import salobj
-from lsst.ts.xml.enums.Guider import GuiderStatus
 
 from . import __version__
 from .config_schema import CONFIG_SCHEMA
 
-# Number of guider sensors reported by the Guider interface.
-# Matches the Count: 8 used in Guider_Commands.xml /
-# Guider_Events.xml for roiXLeft, roiXRight, roiYBottom,
-# roiYTop and status.
-NUMBER_OF_GUIDER_SENSORS = 8
-
 
 class GuiderCsc(salobj.ConfigurableCsc):
     """Guider CSC.
+
+    The Guider interface provides the standard CSC commands only.
 
     Parameters
     ----------
@@ -82,8 +77,6 @@ class GuiderCsc(salobj.ConfigurableCsc):
         index=None,
     ):
         self.config = None
-        self._is_guiding = False
-        self._have_started_guiding = False
 
         super().__init__(
             name="Guider",
@@ -100,80 +93,6 @@ class GuiderCsc(salobj.ConfigurableCsc):
 
     async def configure(self, config):
         self.config = config
-
-    async def start(self):
-        await super().start()
-        await self.evt_guidingStatus.set_write(
-            status=[GuiderStatus.STOPPED] * NUMBER_OF_GUIDER_SENSORS,
-        )
-
-    async def begin_disable(self, data):
-        self._is_guiding = False
-        self._have_started_guiding = False
-        await self.evt_guidingStatus.set_write(
-            status=[GuiderStatus.STOPPED] * NUMBER_OF_GUIDER_SENSORS,
-        )
-
-    async def do_startGuiding(self, data):
-        """Start guiding using the input information.
-
-        The command returns as soon as guiding starts,
-        before the first iteration is completed. If it
-        fails to find a guide star in the provided guider
-        region, the CSC will go to fault state.
-
-        Parameters
-        ----------
-        data : ``cmd_startGuiding.DataType``
-            Command data with the following fields:
-
-            * roiXLeft (long[8], pixel): origin of the
-              guider ROI window in x for each of the 8
-              guider sensors.
-            * roiXRight (long[8], pixel): end of the
-              guider ROI window in x for each sensor.
-              roiXLeft == roiXRight == 0 means no guider
-              region for that sensor.
-            * roiYBottom (long[8], pixel): origin of the
-              guider ROI window in y for each sensor.
-            * roiYTop (long[8], pixel): end of the guider
-              ROI window in y for each sensor. Same
-              "both zero means disabled" convention as x.
-            * expTime (float, ms): exposure time; common
-              to all guider sensors.
-            * binning (long, pixel): binning factor;
-              common to all guider sensors.
-        """
-        self.assert_enabled()
-        raise NotImplementedError("Start Guiding is not implemented yet.")
-
-    async def do_stopGuiding(self, data):
-        """Stop current guider operation.
-
-        Parameters
-        ----------
-        data : ``cmd_stopGuiding.DataType``
-            Command data (no parameters).
-        """
-        self.assert_enabled()
-        raise NotImplementedError("Stop Guiding is not implemented yet.")
-
-    async def do_resumeGuiding(self, data):
-        """Resume current guider operation.
-
-        This command will restart a guider operation
-        with the last values used in startGuiding. If
-        no previous startGuiding command was issued
-        since the last time the CSC was enabled, the
-        command will be rejected.
-
-        Parameters
-        ----------
-        data : ``cmd_resumeGuiding.DataType``
-            Command data (no parameters).
-        """
-        self.assert_enabled()
-        raise NotImplementedError("Resume Guiding is not implemented yet.")
 
 
 def run_guider_csc() -> None:
