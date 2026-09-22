09f26679084257d638f8fec1737243fda02a4d03
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2026-09-22 14:56:50 -0700

    Guider source detection utility
    .- seed reference-image construction.
    .- source candidate detection.
    .- annulus background subtraction.

--- python/lsst/ts/guider/pipeline/__init__.py
+++ python/lsst/ts/guider/pipeline/__init__.py
@@ -19,4 +19,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+from .detection import *
 from .guider_tracker_config import *
